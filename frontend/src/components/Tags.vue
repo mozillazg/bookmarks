70aca3ff631e8e40d735250f43e65ef1369b97e6
mozillazg <mozillazg101@gmail.com>
2016-11-10 22:22:25 +0800

    add tags

---
+++ frontend/src/components/Tags.vue
@@ -0,0 +1,34 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <span v-for="tag in tags">
+        <span class="tag is-info">{{ tag.name }}</span>
+      </span>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Tags',
+  data: function () {
+    return {
+      tags: []
+    }
+  },
+  created: function () {
+    this.fetchData()
+  },
+  methods: {
+    fetchData: function () {
+      this.fetch()
+    },
+    fetch: function () {
+      const url = '/api/v1/tags'
+      this.$http.get(url).then((response) => {
+        this.tags = response.body
+      })
+    }
+  }
+}
+</script>

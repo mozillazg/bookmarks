88962385152ff9a8e3bb8546aa6d481e36029b81
mozillazg <mozillazg101@gmail.com>
2016-11-10 22:22:33 +0800

    add categories

---
+++ frontend/src/components/Categories.vue
@@ -0,0 +1,34 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <span v-for="category in categories">
+        <span class="tag is-info">{{ category.name }}</span>
+      </span>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Categories',
+  data: function () {
+    return {
+      categories: []
+    }
+  },
+  created: function () {
+    this.fetchData()
+  },
+  methods: {
+    fetchData: function () {
+      this.fetch()
+    },
+    fetch: function () {
+      const url = '/api/v1/categories'
+      this.$http.get(url).then((response) => {
+        this.categories = response.body
+      })
+    }
+  }
+}
+</script>
